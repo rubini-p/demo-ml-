3ceab28530c1a038dd79ab14ebfbd2a189348091
Pablo Rubini <pablorubini@gmail.com>
2023-07-18 16:42:37 +0200

    new file:   HELP.md

---
+++ src/main/java/com/ml/demoapp/controller/DemoAppController.java
@@ -0,0 +1,12 @@
+package com.ml.demoapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoAppController {
+  @GetMapping
+  public String hello() {
+    return "Hello javaTpoint";
+  }
+}
